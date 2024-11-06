ef01b6f6103ffa8acee43d79ff66489a211d6ec3
Mel <mel.peslier@gmail.com>
2024-11-06 11:22:47 -0300

    Create card_group_data.gd

---
+++ all/cards/resources/card_group_data.gd
@@ -0,0 +1,9 @@
+class_name CardGroupData
+extends Resource
+
+@export var titre : String
+@export var mots_cles : String
+@export var description : String
+@export var image : Texture2D
+@export var nombre_de_cartes : int
+@export var cards_data : Array[CardData]

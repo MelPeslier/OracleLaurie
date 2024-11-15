3edbed703d67160684ce31b1ab304e1fd62a766c
Mel <mel.peslier@gmail.com>
2024-11-15 11:46:23 -0300

    no texxt fo hide buttons

--- all/menu/components/custom_button/hide_button.gd
+++ all/menu/components/custom_button/hide_button.gd
@@ -1,41 +1,41 @@
 class_name HideButton
 extends BaseCustomButton
 
-@export var text_left := false : set = _set_text_left
-
+#@export var text_left := false : set = _set_text_left
+#@export var angle_left : float = 0.5
+#@export var angle_right : float = 0.0
 
 @onready var h_box: HBoxContainer = $HBox
 @onready var main: TextureRect = %Main
 
-@onready var svc: SubViewportContainer = %SVC
-@onready var label: Label = %Label
+#@onready var svc: SubViewportContainer = %SVC
+#@onready var label: Label = %Label
 
 func _ready() -> void:
 	super()
-	text = text
+	#text = text
 	texture = texture
-	text_left = text_left
+	#text_left = text_left
 
 
-func update_size() -> void:
-	svc.custom_minimum_size = label.size
-	print(label.position)
-	label.position = Vector2.ZERO
 
 
-func _set_text_left(_text_left: bool) -> void:
-	text_left = _text_left
-	if not is_inside_tree(): return
-	if text_left:
-		h_box.move_child(svc, 0)
-		print("moved")
 
-func _set_text(_text: String) -> void:
-	super( _text )
-	if not is_inside_tree(): return
-	label.text = _text
-	#label.set_anchors_preset.call_deferred(Control.PRESET_CENTER)
-	update_size.call_deferred()
+#func _set_text_left(_text_left: bool) -> void:
+	#text_left = _text_left
+	#if not is_inside_tree(): return
+	#var mat: ShaderMaterial = svc.material
+	#mat.set_shader_parameter("angle_coef", angle_right)
+	#if text_left:
+		#h_box.move_child(svc, 0)
+		#label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+		#mat.set_shader_parameter("angle_coef", angle_left)
+
+
+#func _set_text(_text: String) -> void:
+	#super( _text )
+	#if not is_inside_tree(): return
+	#label.text = _text
 
 func _set_texture(_texture: Texture2D) -> void:
 	super( _texture )
